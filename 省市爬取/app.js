8911d6bd1e0e39bf19214bd0958acf2ad675f472
xingxiren <xiongliwei-2004@163.com>
2017-03-02 18:01:24 +0800

    修改

--- 省市爬取/app.js
+++ 省市爬取/app.js
@@ -1,9 +1,14 @@
 /**
  * Created by Administrator on 2017/2/27.
  */
+
+//获取省市
 var provinceCityCrawler = require('./provinceCityCrawler.js');
+//挑号网
 var numberSegmentByTiaoHao = require('./numberSegmentByTiaoHao.js');
+//从本地文件获取号码段
 var numberSegmentByLocal = require('./numberSegmentByLocal.js');
+//根据地区分类号码段
 var screenNumberRegion = require('./screenNumberRegion.js');
 
 //provinceCityCrawler.run();
