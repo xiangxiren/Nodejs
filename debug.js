1e140f5732a618ee69cd3d5a6d39476b190cbd12
xingxiren <xiongliwei-2004@163.com>
2017-03-15 17:37:38 +0800

    新增for和for in 测试

--- debug.js
+++ debug.js
@@ -1,7 +1,24 @@
 var a = 1;
 var b = "world";
-var c = function (x){
-	console.log('hello ' + x + a);
+var c = function (x) {
+    console.log('hello ' + x + a);
 }
 
 c(b);
+
+let person = {name: 'John', age: 20};
+for (let attr in person) {
+    console.log(`${attr}:${person[attr]}`);
+}
+
+let arr = [1, 3, 4, 6, 7, 80];
+for (let index in arr) {
+    console.log(`typeof index:${typeof index},arr[${index}]:${arr[index]}`);
+}
+
+Array.prototype.test = function () {
+
+}
+for (let index in arr) {
+    console.log(`typeof index:${typeof index},arr[${index}]:${arr[index]},typeof arr[index]:${typeof arr[index]}`);
+}
